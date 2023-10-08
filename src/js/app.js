b099cd30a50edabbaa13bf33e4793ddcc25c0376
kovadaniel <danielkovalenko50@gmail.com>
2023-10-09 01:33:03 +0300

    Burger menu

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	////////////////////////////////
 	// Burger
-	document.getElementById('burger').addEventListener('click', function () {
-		const menu = document.getElementById('burger-menu')
-		menu.classList.toggle('show')
+	document.getElementById('burger').addEventListener('click', function (e) {
+		e.currentTarget.classList.toggle('open');
+		const menu = document.getElementById('burger-menu');
+		menu.classList.toggle('show');
 	});
 
 	////////////////////////////////
